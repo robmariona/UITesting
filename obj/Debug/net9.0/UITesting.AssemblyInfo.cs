4dfec0e82500f13ef9a7eba89b773cedcb654fe3
robmariona <roberto.rmariona@gmail.com>
2026-05-15 00:43:22 -0600

    first commit 3

--- obj/Debug/net9.0/UITesting.AssemblyInfo.cs
+++ obj/Debug/net9.0/UITesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UITesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3111f028399945f18e48a914c03b74916fd4cf36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ccc585665bc2c070a61d4757720d171838239f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UITesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UITesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
